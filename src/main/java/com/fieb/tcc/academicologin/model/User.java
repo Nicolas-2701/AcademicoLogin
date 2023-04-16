1d7a09d40a1ff1a1752f4f689eb776c8a548f302
Nicolas-2701 <ngalvao388@gmail.com>
2023-04-16 19:37:29 -0300

    tarefa feita

---
+++ src/main/java/com/fieb/tcc/academicologin/model/User.java
@@ -0,0 +1,166 @@
+package com.fieb.tcc.academicologin.model;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+  
+	@Id // Chave Primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-Incremento
+	private Long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	@Column (name="last_name")
+	private String lastName;
+	private String email;
+	private String password;
+	private String address;
+	private String district;
+	private String cep;
+	private String number;
+	private String city;
+	private String state;
+	private String country;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			  name="users_roles",
+			  joinColumns = @JoinColumn(name="user_id", referencedColumnName = "id"),
+			  inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName = "id")
+			)
+	private Collection<Role> roles;
+	
+	public User() {
+		
+	}
+
+	public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
---
+++ src/test/java/com/fieb/academicologin/AcademicologinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fieb.academicologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicologinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
